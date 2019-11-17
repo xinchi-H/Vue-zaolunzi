3efeb6e3bc691f551d1ddb74e33a9a15618144a4
huxc <455908283@qq.com>
2019-11-18 03:41:37 +0800

    单元测试

--- src/app.js
+++ src/app.js
@@ -3,9 +3,9 @@ import Button from './button'
 import Icon from './icon'
 import ButtonGroup from './button-group'
 
-Vue.component('g-button', Button)
-Vue.component('g-icon', Icon)
-Vue.component('g-button-group', ButtonGroup)
+Vue.component('g-button', Button);
+Vue.component('g-icon', Icon);
+Vue.component('g-button-group', ButtonGroup);
 
 new Vue({
     el: '#app',
@@ -14,4 +14,21 @@ new Vue({
         loading2: true,
         loading3: false,
     }
-})
+});
+
+
+//单元测试
+import chai from 'chai'
+const expect = chai.expect;
+{
+    const Constructor = Vue.extend(Button);
+    const button = new Constructor({
+        propsData:{
+            icon:'settings'
+        }
+    });
+    button.$mount();
+    let useElement = button.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-settings')
+}
