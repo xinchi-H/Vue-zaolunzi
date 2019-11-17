093f2e82026cac5bc83f3c7b200ae09b22bfb137
huxc <455908283@qq.com>
2019-11-17 20:04:29 +0800

    添加click事件

--- src/app.js
+++ src/app.js
@@ -2,9 +2,14 @@ import Vue from 'vue'
 import Button from './button'
 import Icon from './icon'
 
-Vue.component('g-button',Button)
-Vue.component('g-icon',Icon)
+Vue.component('g-button', Button)
+Vue.component('g-icon', Icon)
 
 new Vue({
     el: '#app',
+    data: {
+        loading1: false,
+        loading2: true,
+        loading3: false,
+    }
 })
