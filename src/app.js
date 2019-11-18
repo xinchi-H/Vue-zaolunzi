bb6cf387f4b799e46d4a41367baa2357258c366a
huxc <455908283@qq.com>
2019-11-18 20:03:16 +0800

    更改实例button名称为vm

--- src/app.js
+++ src/app.js
@@ -23,63 +23,77 @@ import chai from 'chai'
 const expect = chai.expect;
 {
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings'
         }
     });
-    button.$mount();
-    let useElement = button.$el.querySelector('use');
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
     let href = useElement.getAttribute('xlink:href');
     expect(href).to.eq('#i-settings');
-    button.$el.remove();
-    button.$destroy();
+    vm.$el.remove();
+    vm.$destroy();
 }
 {
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings',
             loading: true,
         }
     });
-    button.$mount();
-    let useElement = button.$el.querySelector('use');
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
     let href = useElement.getAttribute('xlink:href');
     expect(href).to.eq('#i-loading');
-    button.$el.remove();
-    button.$destroy();
+    vm.$el.remove();
+    vm.$destroy();
 }
 {
     const div = document.createElement('div');
     document.body.appendChild(div);
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings',
         }
     });
-    button.$mount(div); //不挂载到页面，就不会渲染这个svg，就没有css
-    let svg = button.$el.querySelector('svg');
+    vm.$mount(div); //不挂载到页面，就不会渲染这个svg，就没有css
+    let svg = vm.$el.querySelector('svg');
     let order = window.getComputedStyle(svg).order;
     expect(order).to.eq('1');
-    button.$el.remove();
-    button.$destroy();
+    vm.$el.remove();
+    vm.$destroy();
 }
 {
     const div = document.createElement('div');
     document.body.appendChild(div);
     const Constructor = Vue.extend(Button);
-    const button = new Constructor({
+    const vm = new Constructor({
         propsData: {
             icon: 'settings',
             iconPosition: 'right',
         }
     });
-    button.$mount(div); //不挂载到页面，就不会渲染这个svg，就没有css
-    let svg = button.$el.querySelector('svg');
+    vm.$mount(div); //不挂载到页面，就不会渲染这个svg，就没有css
+    let svg = vm.$el.querySelector('svg');
     let order = window.getComputedStyle(svg).order;
     expect(order).to.eq('2');
-    button.$el.remove();
-    button.$destroy();
+    vm.$el.remove();
+    vm.$destroy();
+}
+{
+    const Constructor = Vue.extend(Button);
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings'
+        }
+    });
+    vm.$mount();
+    let useElement = vm.$el.querySelector('use');
+    let href = useElement.getAttribute('xlink:href');
+    expect(href).to.eq('#i-settings');
+    vm.$el.remove();
+    vm.$destroy();
 }
