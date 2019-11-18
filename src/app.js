36be3fe2d9cb30fc0d04f87241caaa89146a9d47
huxc <455908283@qq.com>
2019-11-18 20:36:47 +0800

    使用spy监听回调函数

--- src/app.js
+++ src/app.js
@@ -19,6 +19,8 @@ new Vue({
 
 //单元测试
 import chai from 'chai'
+import spies from 'chai-spies'
+chai.use(spies);
 
 const expect = chai.expect;
 {
@@ -91,9 +93,12 @@ const expect = chai.expect;
         }
     });
     vm.$mount();
-    let useElement = vm.$el.querySelector('use');
-    let href = useElement.getAttribute('xlink:href');
-    expect(href).to.eq('#i-settings');
+    let spy = chai.spy(function () {});
+    vm.$on('click',spy);
+    //希望这个函数被执行
+    let button = vm.$el;
+    button.click();
+    expect(spy).to.have.been.called();
     vm.$el.remove();
     vm.$destroy();
 }
