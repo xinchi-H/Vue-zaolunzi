fcfb8e35aa0656b236a7eaece63228e3ad86420e
huxc <455908283@qq.com>
2019-11-20 21:09:49 +0800

    input css

--- src/input.vue
+++ src/input.vue
@@ -1,12 +1,17 @@
 <template>
     <div class="wrapper">
-        <input type="text">
+        <input :value="value" type="text">
     </div>
 </template>
 
 <script>
     export default {
         name: 'lunzi-input',
+        props: {
+            value: {
+                type: String
+            }
+        }
     }
 </script>
 
@@ -21,10 +26,27 @@
     /*--border-color-hover: #666;*/
     $height: 32px;
     $border-color: #999;
+    $border-color-hover: #666;
+    $border-radius: 4px;
+    $font-size: 12px;
+    $box-shadow-color: rgba(0,0,0,0.5);
     .wrapper {
+        font-size: $font-size;
+        display: inline-block;
         > input {
             height: $height;
             border: 1px solid $border-color;
+            border-radius: 4px;
+            padding: 0 8px;
+
+            &:hover {
+                border-color: $border-color-hover;
+            }
+
+            &:focus {
+                outline: none;
+                box-shadow: inset 0 1px 3px $box-shadow-color;
+            }
         }
     }
 </style>
