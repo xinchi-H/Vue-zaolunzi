08196610a157cb2e00701a372aac1cef96a9f79d
huxc <455908283@qq.com>
2019-11-20 22:34:13 +0800

    input css

--- src/input.vue
+++ src/input.vue
@@ -1,14 +1,32 @@
 <template>
-    <div class="wrapper">
-        <input :value="value" type="text">
+    <div class="wrapper" :class="{error}">
+        <input :value="value" type="text" :disabled="disabled" :readonly="readonly">
+        <template v-if="error">
+            <icon name="error" class="icon-error"></icon>
+            <span class="errorMessage">{{error}}</span>
+        </template>
     </div>
 </template>
 
 <script>
+    import Icon from './icon'
+
     export default {
+        components: {Icon},
         name: 'lunzi-input',
         props: {
             value: {
+                type: String
+            },
+            disabled: {
+                type: Boolean,
+                default: false,
+            },
+            readonly: {
+                type: Boolean,
+                default: false,
+            },
+            error: {
                 type: String
             }
         }
@@ -16,37 +34,28 @@
 </script>
 
 <style lang="scss" scoped>
-    /*--button-height: 32px;*/
-    /*--font-size: 14px;*/
-    /*--button-background: white;*/
-    /*--button-active-background: #eee;*/
-    /*--border-radius: 4px;*/
-    /*--color: #333;*/
-    /*--border-color: #999;*/
-    /*--border-color-hover: #666;*/
     $height: 32px;
     $border-color: #999;
     $border-color-hover: #666;
     $border-radius: 4px;
     $font-size: 12px;
-    $box-shadow-color: rgba(0,0,0,0.5);
-    .wrapper {
-        font-size: $font-size;
-        display: inline-block;
-        > input {
-            height: $height;
-            border: 1px solid $border-color;
-            border-radius: 4px;
-            padding: 0 8px;
-
-            &:hover {
-                border-color: $border-color-hover;
-            }
-
-            &:focus {
-                outline: none;
-                box-shadow: inset 0 1px 3px $box-shadow-color;
-            }
+    $box-shadow-color: rgba(0, 0, 0, 0.5);
+    $red: #F1453D;
+    .wrapper {font-size: $font-size;display: inline-flex;
+        align-items: center;
+        > :not(:last-child) {margin-right: 0.5em;
+            /*> * {margin-right: 0.5em;*/
+            /*    &:last-child {margin-right: 0;}*/
         }
+        > input {height: $height;border: 1px solid $border-color;border-radius: 4px;padding: 0 8px;
+            &:hover {border-color: $border-color-hover;}
+            &:focus {outline: none;box-shadow: inset 0 1px 3px $box-shadow-color;}
+            &[disabled], &[readonly] {border-color: #bbb;color: #bbb;cursor: not-allowed;}
+        }
+        &.error {
+            > input {border-color: $red;}
+        }
+        .icon-error {fill: $red;}
+        .errorMessage {color: $red;}
     }
 </style>
