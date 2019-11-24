07d16225204a9a3f13331e66d90bc25c4536acc7
huxc <455908283@qq.com>
2019-11-24 23:25:07 +0800

    gutter

--- src/col.vue
+++ src/col.vue
@@ -1,6 +1,9 @@
 <template>
-    <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]">
-        <slot></slot>
+    <div class="col" :class="[`span && col-${span}`,offset && `offset-${offset}`]"
+         :style="{paddingLeft: gutter/2+'px',paddingRight: gutter/2+'px'}">
+        <div style="border: 1px solid green;height: 100px">
+            <slot></slot>
+        </div>
     </div>
 </template>
 <script>
@@ -12,16 +15,18 @@
             },
             offset: {
                 type: [Number, String]
+            },
+        },
+        data() {
+            return {
+                gutter: 0
             }
-        }
+        },
     }
 </script>
 <style scoped lang="scss">
     .col {
-        height: 100px;
-        background: grey;
         width: 50%;
-        border: 1px solid red;
         $class: col-;
         @for $n from 1 through 24 {
             &.#{$class}#{$n} {
--- src/row.vue
+++ src/row.vue
@@ -1,9 +1,24 @@
 <template>
-    <div class="row">
+    <div class="row" :style="{marginLeft: -gutter/2+'px',marginRight: -gutter/2+'px'}">
         <slot></slot>
     </div>
 </template>
-
+<script>
+    export default {
+        name: 'lunzi-row',
+        props: {
+            gutter: {
+                type: [Number, String]
+            }
+        },
+        mounted() {
+            console.log(this.$children);
+            this.$children.forEach((vm) => {
+                vm.gutter = this.gutter
+            })
+        }
+    }
+</script>
 <style scoped lang="scss">
     .row {
         display: flex;
