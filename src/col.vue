93e8c91b1015263ca012b9ab4c4c9926d7a56bab
huxc <455908283@qq.com>
2019-11-24 22:16:46 +0800

    offset

--- src/col.vue
+++ src/col.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="col" :class="[`col-${span}`]">
+    <div class="col" :class="[`col-${span}`,offset && `offset-${offset}`]">
         <slot></slot>
     </div>
 </template>
@@ -8,6 +8,9 @@
         name: 'lunzi-col',
         props: {
             span: {
+                type: [Number, String]
+            },
+            offset: {
                 type: [Number, String]
             }
         }
@@ -23,6 +26,12 @@
         @for $n from 1 through 24 {
             &.#{$class}#{$n} {
                 width: ($n/24)*100%;
+            }
+        }
+        $class: offset-;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                margin-left: ($n/24)*100%;
             }
         }
     }
