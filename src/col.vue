a56829c54b4b9ea3defa24b0db7020ed02aea220
huxc <455908283@qq.com>
2019-11-24 21:57:48 +0800

    row和col雏形

---
+++ src/col.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="col" :class="[`col-${span}`]">
+        <slot></slot>
+    </div>
+</template>
+<script>
+    export default {
+        name: 'lunzi-col',
+        props: {
+            span: {
+                type: [Number, String]
+            }
+        }
+    }
+</script>
+<style scoped lang="scss">
+    .col {
+        height: 100px;
+        background: grey;
+        width: 50%;
+        border: 1px solid red;
+        $class: col-;
+        @for $n from 1 through 24 {
+            &.#{$class}#{$n} {
+                width: ($n/24)*100%;
+            }
+        }
+    }
+</style>
