96b2e05a2f4bbdd875e40305a532cbfedbf7a80d
huxc <455908283@qq.com>
2019-12-03 02:47:13 +0800

    toast

--- src/app.js
+++ src/app.js
@@ -11,19 +11,21 @@ import Sider from './sider'
 import Content from './content'
 import Footer from './footer'
 import Toast from './toast'
+import plugin from "./plugin";
 
 Vue.component('g-button', Button);
 Vue.component('g-icon', Icon);
 Vue.component('g-button-group', ButtonGroup);
-Vue.component('g-input',Input);
-Vue.component('g-row',Row);
-Vue.component('g-col',Col);
-Vue.component('g-layout',Layout);
-Vue.component('g-header',Header);
-Vue.component('g-sider',Sider);
-Vue.component('g-content',Content);
-Vue.component('g-footer',Footer);
-Vue.component('g-toast',Toast);
+Vue.component('g-input', Input);
+Vue.component('g-row', Row);
+Vue.component('g-col', Col);
+Vue.component('g-layout', Layout);
+Vue.component('g-header', Header);
+Vue.component('g-sider', Sider);
+Vue.component('g-content', Content);
+Vue.component('g-footer', Footer);
+Vue.component('g-toast', Toast);
+Vue.use(plugin);
 
 new Vue({
     el: '#app',
@@ -33,9 +35,15 @@ new Vue({
         loading3: false,
         message: '王',
     },
-    methods:{
-        inputChange(info){
+    created() {
+
+    },
+    methods: {
+        inputChange(info) {
             console.log(info);
+        },
+        showToast() {
+            this.$toast('我是 message')
         }
     }
 });
