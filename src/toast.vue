aed080370c363484d73686afd960e576c6befb00
huxc <455908283@qq.com>
2019-12-03 23:59:43 +0800

    toast样式

--- src/toast.vue
+++ src/toast.vue
@@ -6,12 +6,28 @@
 
 <script>
     export default {
-        name:'lunzi-toast'
+        name: 'lunzi-toast'
     }
 </script>
 
 <style scoped lang="scss">
-    .toast{
-
+    $font-size: 14px;
+    $toast-height: 40px;
+    $toast-bg: rgba(0, 0, 0, 0.75);
+    .toast {
+        color: white;
+        position: fixed;
+        top: 0;
+        left: 50%;
+        transform: translateX(-50%);
+        font-size: $font-size;
+        line-height: 1.8;
+        height: $toast-height;
+        display: flex;
+        align-items: center;
+        background: $toast-bg;
+        border-radius: 4px;
+        box-shadow: 0 0 3px 0 rgba(0, 0, 0, 0.50);
+        padding: 0 16px;
     }
 </style>
