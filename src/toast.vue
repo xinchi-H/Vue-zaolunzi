4be12b02ea95093515201e472256544dad7c60d6
huxc <455908283@qq.com>
2019-12-05 02:34:05 +0800

    优化三种动画

--- src/toast.vue
+++ src/toast.vue
@@ -1,13 +1,15 @@
 <template>
-    <div class="toast" ref="wrapper" :class="toastClasses">
-        <div class="message">
-            <slot v-if="!enableHTML"></slot>
-            <div v-else v-html="$slots.default[0]"></div>
-        </div>
-        <div class="line" ref="line"></div>
-        <span class="close" v-if="closeButton" @click="onClickClose()">
+    <div class="wrapper" :class="toastClasses">
+        <div class="toast" ref="toast">
+            <div class="message">
+                <slot v-if="!enableHTML"></slot>
+                <div v-else v-html="$slots.default[0]"></div>
+            </div>
+            <div class="line" ref="line"></div>
+            <span class="close" v-if="closeButton" @click="onClickClose()">
             {{closeButton.text}}
         </span>
+        </div>
     </div>
 </template>
 
@@ -59,7 +61,7 @@
             updateStyles() {
                 this.$nextTick(() => {
                     this.$refs.line.style.height =
-                        `${this.$refs.wrapper.getBoundingClientRect().height}px`
+                        `${this.$refs.toast.getBoundingClientRect().height}px`
                 })
             },
             execAutoClose() {
@@ -88,15 +90,47 @@
     $font-size: 14px;
     $toast-min-height: 40px;
     $toast-bg: rgba(0, 0, 0, 0.75);
-    @keyframes fade-in {
-        0% {opacity: 0; transform: translateY(100%)}
-        100% { opacity: 1;transform: translateY(0%)}
+    $animation-duration:300ms;
+    @keyframes slide-down {
+        0% {opacity: 0; transform: translateY(-100%)}
+        100% { opacity: 100%;transform: translateY(0%)}
     }
-    .toast {
-        animation: fade-in 1s;
-        color: white;
+    @keyframes fade-in {
+        0% {opacity: 0;}
+        100% { opacity: 100%;}
+    }
+    @keyframes slide-up {
+        0% {opacity: 0; transform: translateY(100%)}
+        100% { opacity: 100%;transform: translateY(0%)}
+    }
+    .wrapper {
         position: fixed;
         left: 50%;
+        transform: translateX(-50%);
+        &.position-top {
+            top: 0;
+            .toast {
+                border-top-left-radius: 0;
+                border-top-right-radius: 0;
+                animation: slide-down $animation-duration;
+            }
+        }
+        &.position-bottom {
+            bottom: 0;
+            .toast {
+                border-bottom-left-radius: 0;
+                border-bottom-right-radius: 0;
+                animation: slide-up $animation-duration;
+            }
+        }
+        &.position-middle {
+            top: 50%;
+            transform: translateX(-50%) translateY(-50%);
+            .toast { animation: fade-in $animation-duration;}
+        }
+    }
+    .toast {
+        color: white;
         font-size: $font-size;
         line-height: 1.8;
         min-height: $toast-min-height;
@@ -117,18 +151,6 @@
             height: 100%;
             border-left: 1px solid #666;
             margin-left: 16px;
-        }
-        &.position-top {
-            top: 0;
-            transform: translateX(-50%);
-        }
-        &.position-bottom {
-            bottom: 0;
-            transform: translateX(-50%);
-        }
-        &.position-middle {
-            top: 50%;
-            transform: translate(-50%, -50%);
         }
     }
 </style>
