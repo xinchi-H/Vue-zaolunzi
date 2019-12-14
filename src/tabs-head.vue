d963c309382d42aac6339d706d1d47e84f5e5ef7
huxc <455908283@qq.com>
2019-12-15 02:58:29 +0800

    完善tabs的样式

--- src/tabs-head.vue
+++ src/tabs-head.vue
@@ -25,19 +25,25 @@
 <style scoped lang="scss">
     $blue: blue;
     $tab-height: 40px;
+    $border-color: #ddd;
     .tabs-head {
         display: flex;
         height: $tab-height;
         justify-content: flex-start;
         position: relative;
+        border-bottom: 1px solid $border-color;
         > .line {
             position: absolute;
             bottom: 0;
             border-bottom: 1px solid $blue;
-            transition: all 360ms;
+            transition: all 350ms;
         }
         .actions-wrapper {
             margin-left: auto;
+            display: flex;
+            align-items: center;
+            justify-content: center;
+            padding: 0 1em;
         }
     }
 </style>
--- src/tabs-pane.vue
+++ src/tabs-pane.vue
@@ -36,8 +36,9 @@
 
 <style scoped lang="scss">
     .tabs-pane {
+        padding: 1em;
         &.active{
-            background: red;
+
         }
     }
 </style>
