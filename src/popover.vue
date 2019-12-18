c60c9cf5dc618a1b4c92c18ed22042c270339a3d
huxc <455908283@qq.com>
2019-12-19 00:48:53 +0800

    popover

--- src/popover.vue
+++ src/popover.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="popover" @click.stop="xxx">
+    <div class="popover" @click="onClick" ref="popover">
         <div ref="contentWrapper" class="content-wrapper" v-if="visible">
             <slot name="content"></slot>
         </div>
@@ -13,27 +13,43 @@
     export default {
         name: "lunzi-popover",
         data() {
-            return {
-                visible: false
-            }
+            return {visible: false}
         },
         methods: {
-            xxx() {
-                this.visible = !this.visible;
-                if (this.visible === true) {
-                    this.$nextTick(() => {
-                        document.body.appendChild(this.$refs.contentWrapper);
-                        let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
-                        this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
-                        this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
-                        let eventHandle = () => {
-                            this.visible = false;
-                            document.removeEventListener('click', eventHandle)
-                        };
-                        document.addEventListener('click', eventHandle)
-                    })
+            onClick(event) {
+                if (this.$refs.triggerWrapper.contains(event.target)) {
+                    if (this.visible === true) {
+                        this.close()
+                    } else {
+                        this.open()
+                    }
                 }
-            }
+            },
+            close() {
+                this.visible = false;
+                document.removeEventListener('click', this.onClickDocument)
+            },
+            open() {
+                this.visible = true;
+                this.$nextTick(() => {
+                    this.positionContent();
+                    document.addEventListener('click', this.onClickDocument)
+                })
+            },
+            positionContent() {
+                document.body.appendChild(this.$refs.contentWrapper);
+                let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect();
+                this.$refs.contentWrapper.style.left = left + window.scrollX + 'px';
+                this.$refs.contentWrapper.style.top = top + window.scrollY + 'px';
+            },
+            onClickDocument(e) {
+                if (this.$refs.popover &&
+                    (this.$refs.popover === e.target || this.$refs.popover.contains(e.target))) {
+                    return
+                }
+                console.log('d');
+                this.close();
+            },
         }
     }
 </script>
